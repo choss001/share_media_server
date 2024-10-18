f267b4e5f443c6472536927722a59090a4a768fe
choss001 <choss01@naver.com>
2024-10-18 15:17:22 +0900

    share project initial

---
+++ src/main/java/com/media/share/ShareMediaApplication.java
@@ -0,0 +1,13 @@
+package com.media.share;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShareMediaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShareMediaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/media/share/ShareMediaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.media.share;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShareMediaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
