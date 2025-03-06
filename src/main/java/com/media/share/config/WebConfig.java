0472ae6179d144ca34fe46464a2ed212f6e20192
choss001 <choss01@naver.com>
2025-03-06 20:16:45 +0900

    add cors origin a31.ddns.net:80, 3000

--- src/main/java/com/media/share/config/WebConfig.java
+++ src/main/java/com/media/share/config/WebConfig.java
@@ -10,8 +10,9 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins(
-                        "http://localhost:3001",
-                        "http://183.102.80.161:3000"
+                        "http://a31.ddns.net:80",
+                        "http://a31.ddns.net:3000",
+                        "http://localhost:3001"
                 ) // Next.js default port
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
     }
