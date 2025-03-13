511b59aa00f9deac3d75792502e13846e8c89148
choss001 <choss01@naver.com>
2025-03-13 14:16:36 +0900

    access all origin

--- src/main/java/com/media/share/config/WebConfig.java
+++ src/main/java/com/media/share/config/WebConfig.java
@@ -9,11 +9,12 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins(
-                        "http://a31.ddns.net:80",
-                        "http://a31.ddns.net:3000",
-                        "http://localhost:3001"
-                ) // Next.js default port
+//                .allowedOrigins(
+//                        "http://a31.ddns.net:80",
+//                        "http://a31.ddns.net:3000",
+//                        "http://localhost:3001"
+//                ) // Next.js default port
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
     }
 }
--- src/main/resources/db/migration/V9__add_media_files_column.sql
+++ src/main/resources/db/migration/V9__add_media_files_column.sql
@@ -1,12 +1,14 @@
 ALTER TABLE media_file ADD required_role bigint NOT NULL;
-<!--UPDATE media_file SET required_role = 3;-->
+UPDATE media_file SET required_role = 3;
 ALTER TABLE media_file
 ADD CONSTRAINT fk_required_role
 FOREIGN KEY (required_role)
 REFERENCES sec_role(id);
 
+insert into share_media.user values(1, 'anonymous', 'anonymous@email.com', '$2a$10$FS0iWKmIh3d1QrCCsLypb.Gs/3tH54EPpuqzQdQFBQ3kieww4xYiy');
+
 ALTER TABLE media_file ADD user_id bigint NOT NULL;
-<!--UPDATE media_file SET user_id = (SELECT id FROM `user` u LIMIT 1);-->
+UPDATE media_file SET user_id = (SELECT id FROM `user` u LIMIT 1);
 ALTER TABLE media_file
 ADD CONSTRAINT fk_user_id
 FOREIGN KEY (user_id)
