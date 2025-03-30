14b614b19706dc5a2c019a1ad9a42a82e03d6b30
choss001 <choss01@naver.com>
2025-03-31 05:18:41 +0900

    compile error

--- src/main/java/com/media/share/config/SecurityConfiguration.java
+++ src/main/java/com/media/share/config/SecurityConfiguration.java
@@ -73,7 +73,7 @@ public class SecurityConfiguration {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3001",
+        configuration.setAllowedOrigins(List.of("http://localhost:3001"
                 ,"https://a31.ddns.net")); // Allow your Next.js frontend
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowCredentials(true); // **Required for cookies**
