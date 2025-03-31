2d305ccdd24c5d5f2d2eed4b5208fa595cccf78d
choss001 <choss01@naver.com>
2025-03-31 13:30:09 +0900

    modify cors

--- src/main/java/com/media/share/config/SecurityConfiguration.java
+++ src/main/java/com/media/share/config/SecurityConfiguration.java
@@ -73,8 +73,7 @@ public class SecurityConfiguration {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3001"
-                ,"https://a31.ddns.net")); // Allow your Next.js frontend
+        configuration.setAllowedOrigins(List.of("http://localhost:3001" ,"https://a31.ddns.net")); // Allow your Next.js frontend
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true); // **Required for cookies**
